14ff546710d9fb48dece58978cf3a6c6958aea5d
TeuzimMG <71942508+TeuzimMG@users.noreply.github.com>
2023-03-24 18:52:20 -0300

    Version 2.0

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -27,7 +27,7 @@ export function Home(){
         Aos.init({duration: 2000}), []
     })
 
-    alert('O curso está dispónivel para venda antecipada por metade do preço, lançamento do curso é estimado para mês 5 porém há a capacidade do desenvolvimento ser prolongado até junho.')
+    // alert('O curso está dispónivel para venda antecipada por metade do preço, lançamento do curso é estimado para mês 5 porém há a capacidade do desenvolvimento ser prolongado até junho.')
 
     return(
         <div>
@@ -112,17 +112,19 @@ export function Home(){
                         </p>
                     </div>
                 </section>
-                {/* <section id='intro6'>
+                <section id='intro6'>
                     <div id="perfilbox">
                         <img src={Pessoa} id='perfilbox-image'/>
                         <div id="perfilbox-content">
-                            <span id='pbc-title1'>Quem é o professor?</span>
-                            <span id='pbc-title2'>Matheus Magalhães</span>
+                            <div id='perfilbox-titles'>
+                                <span id='pbc-title1'>Quem é o professor?</span>
+                                <span id='pbc-title2'>Matheus Magalhães</span>
+                            </div>
                             <span id='pbc-title3'>Eu Matheus Magalhães como jovem programador passei por muitos problemas e errei de mais para chegar no nível em que me encontro, porém foquei no que queria e fiquei
                         cansado de ouvir: "você é jovem de mais para ensinar" ou "você não vai chegar a lugar nenhum com isso", pois bem se passaram 7 anos e aqui estou eu.Nesse curso reuni tudo que aprendi de Python nesses ultimos 7 anos de estudos, e pasmem são conteudos que não acabam mais...</span>
                         </div>
                     </div>
-                </section> */}
+                </section>
             </div>
         </div>
     )
